1aef14279d86ba6a9877bdf8de49b3e6b9b003ff
Tharusha Rasath Hemachandra <ptrhemachandra@students.nsbm.ac.lk>
2025-05-24 10:47:32 +0530

    1st day commit

---
+++ Functions_and_Variables/Calculator.py
@@ -0,0 +1,15 @@
+#Get numbers and calculate 
+x= input ("What's x ? ")
+y= input ("What's y ? ")
+
+z= int(x)+int(y) #converting default string type to integer
+print(z)
+
+#without another variable
+x= int(input ("What's x ? "))
+y= int(input ("What's y ? "))
+
+print(x+y)
+
+#one line code
+print (int(input ("What's x ? ")) + int(input ("What's y ? ")) )
--- Functions_and_Variables/hello.py
+++ Functions_and_Variables/hello.py
@@ -1,1 +1,28 @@
-print("hello, world")
+#Ask user the name
+name = input("What's your name? \n")
+#Greeting hello to user
+print("hello", name)
+print ("hello" +name)
+print("hello! ", end="") #end with no new line #default is\n
+print(name)
+print("hello!", name , sep="1213") #seperate with something else rather than default single space
+
+print('hello! "friend"') #quotation inside quotation
+print(f"hello, {name}") #f strings (special type of strings)
+#remove whitespace from str
+name = name.strip()
+#Capitalize
+name = name.capitalize()#1st letter  capitalize
+print ("hello", name)
+name = name.title()  #all 1st letters capitalize
+print ("hello", name)
+name = name.strip().title()# all in one
+print ("hello", name)
+
+name = input("What's your name? ").strip().title() #one line code
+print ("hello", name)
+
+#split user's name into first name and last name\
+name = input("What's ur name? ").strip().title()
+first, last = name.split(" ")
+print (f"hello, {first}")
