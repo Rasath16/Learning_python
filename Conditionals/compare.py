cb2a7557a5acb771b16c541d7d19ca71a4f60004
Tharusha Rasath Hemachandra <ptrhemachandra@students.nsbm.ac.lk>
2025-05-25 19:54:52 +0530

    conditionals first commit

---
+++ Conditionals/compare.py
@@ -0,0 +1,30 @@
+""" x = int(input("What is x : "))
+y = int(input("What is y : "))
+#compare two numbers
+if x < y :
+    print("x is less than y")
+
+if x > y :
+    print("x is greater than y")
+
+if x == y :
+    print("x is equal to y")"""
+
+#using elif
+x = int(input("What is x : "))
+y = int(input("What is y : "))
+
+if x < y :
+    print("x is less than y")
+
+elif x > y :
+    print("x is greater than y")
+
+elif x == y :
+    print("x is equal to y")
+
+
+
+
+
+
---
+++ Conditionals/mine.py
@@ -0,0 +1,21 @@
+#compare 2 numbers run again and again
+def compare():
+
+  x = int(input("What is x : "))
+  y = int(input("What is y : "))
+#compare two numbers
+  if x < y :
+    print("x is less than y")
+
+  if x > y :
+    print("x is greater than y")
+
+  if x == y :
+    print("x is equal to y")
+
+  n = input("Run again y/n ? ")
+
+  if n == "y":
+    compare()
+
+compare()
