cf11919b82c8233afb7db541a08ab1ace22f20b4
Tharusha Rasath Hemachandra <ptrhemachandra@students.nsbm.ac.lk>
2025-05-26 17:07:18 +0530

    update conditionals

--- Conditionals/mine.py
+++ Conditionals/mine.py
@@ -19,3 +19,11 @@ def compare():
     compare()
 
 compare()
+
+x = int(input("What is x : "))
+y = int(input("What is y : "))
+
+if x<y or x>y :
+  print ("x is not equal to y")
+else:
+  print ("x is equal to y")
